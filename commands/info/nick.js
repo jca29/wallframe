678de0054aa0020df0ffbe02e40c47e511e7030f
josuecahuana <jcahuanaag@unsa.edu.pe>
2022-01-19 16:10:49 -0500

    cree comando nick

---
+++ commands/info/nick.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "nick",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async({client, message, args}) => {
+
+        message.reply(message.author.username+" "+message.author.tag)
+    }
+}
