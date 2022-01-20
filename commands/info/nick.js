8b5c185344bae291508964c925639b028657a597
josuecahuana <jcahuanaag@unsa.edu.pe>
2022-01-20 09:36:37 -0500

    modifique nick

--- commands/info/nick.js
+++ commands/info/nick.js
@@ -4,20 +4,10 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async({client, message, ...args}) => {
-        /*if(arguments.length==4){
-            message.member.setNickname(args[1])
-        }
-        else{
-            message.reply("Sintaxis incorrecta, sintaxis correcta: w.nick <nombre>")
-        }*/
         if(args[0]==undefined||args[1]!==undefined){
             message.reply("Sintaxis incorrecta, sintaxis correcta: w.nick <nombre>")
-            console.log(args[0])
-            console.log(args[1])
-            console.log(args[2])
         }else{
             message.member.setNickname(args[0])
         }
-        message.reply(message.author.username+" "+message.author.tag+" "+arguments.length)
     }
 }
