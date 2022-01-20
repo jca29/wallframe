a9e0d6d5cb716ac3286d53cf312cf980d86969ff
josuecahuana <jcahuanaag@unsa.edu.pe>
2022-01-20 09:32:52 -0500

    termine el comando nick

--- commands/info/nick.js
+++ commands/info/nick.js
@@ -4,7 +4,20 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async({client, message, ...args}) => {
-
-        message.reply(message.author.username+" "+message.author.tag+" "+args[0])
+        /*if(arguments.length==4){
+            message.member.setNickname(args[1])
+        }
+        else{
+            message.reply("Sintaxis incorrecta, sintaxis correcta: w.nick <nombre>")
+        }*/
+        if(args[0]==undefined||args[1]!==undefined){
+            message.reply("Sintaxis incorrecta, sintaxis correcta: w.nick <nombre>")
+            console.log(args[0])
+            console.log(args[1])
+            console.log(args[2])
+        }else{
+            message.member.setNickname(args[0])
+        }
+        message.reply(message.author.username+" "+message.author.tag+" "+arguments.length)
     }
 }
